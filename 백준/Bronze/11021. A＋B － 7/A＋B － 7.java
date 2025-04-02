72da14d52fc630c15959fec817d5ffff5d31f215
hayleyun <98436386+hayleyun@users.noreply.github.com>
2025-04-02 20:22:03 +1000

    [Bronze V] Title: A+B - 7, Time: 136 ms, Memory: 16088 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.lang.StringBuilder;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+        
+        StringBuilder sb = new StringBuilder();
+        
+        for (int i = 1; i <= t; i++) {
+            String[] a = br.readLine().split(" ");
+            sb.append("Case #" + i + ": " + (Integer.parseInt(a[0]) + Integer.parseInt(a[1])) + "\n");
+        }
+        
+        System.out.print(sb.toString());
+        
+        br.close();
+        
+    }
+}
