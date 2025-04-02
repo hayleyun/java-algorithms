e62dc2c3f6098e597deb2e8dc00c18444621a51a
hayleyun <98436386+hayleyun@users.noreply.github.com>
2025-04-02 21:00:58 +1000

    [Bronze IV] Title: 별 찍기 - 2, Time: 104 ms, Memory: 14280 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.lang.StringBuilder;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(br.readLine());
+        
+        StringBuilder sb = new StringBuilder();
+        
+        for (int i = 1; i <= a; i++) {
+            for (int j = 1; j <= a - i; j++) {
+                sb.append(" ");
+            }
+            for (int k = 1; k <= i; k++) {
+                sb.append("*");
+            }
+            sb.append("\n");
+        }
+        
+        System.out.print(sb.toString());
+        
+        br.close();
+        
+    }
+}
