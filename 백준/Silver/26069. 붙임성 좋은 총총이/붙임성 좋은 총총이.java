76c6aec750c2ddb3398edee5a55e9a63f386e95a
hayleyun <98436386+hayleyun@users.noreply.github.com>
2025-04-29 12:52:13 +1000

    [Silver IV] Title: 붙임성 좋은 총총이, Time: 128 ms, Memory: 15752 KB -BaekjoonHub

---
+++ 백준/Silver/26069. 붙임성 좋은 총총이/붙임성 좋은 총총이.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        
+        Set<String> set = new HashSet<>();
+        set.add("ChongChong");
+        for (int i = 0; i < n; i++) {
+            String[] input = br.readLine().split(" ");
+            if (set.contains(input[0]) || set.contains(input[1])) {
+                set.add(input[0]);
+                set.add(input[1]);
+            }
+        }
+        
+        System.out.print(set.size());
+        br.close();
+    }
+}
